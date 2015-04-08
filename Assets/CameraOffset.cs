a3d1f73d1c1789f5ec9295b9686ca08137df9011
Shevis Johnson <johns612@mail.chapman.edu>
2015-04-07 20:10:28 -0700

    Unity_Project

---
+++ Assets/CameraOffset.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOffset : MonoBehaviour {
+
+	public Transform offset;
+	public Transform notOffset;
+	public bool isOffset = false;
+
+	// Use this for initialization
+	void Start () {	
+
+		offset.localPosition = new Vector3(-2.23f, transform.localPosition.y, transform.localPosition.z);
+		notOffset.localPosition = new Vector3(2.23f, transform.localPosition.y, transform.localPosition.z);
+		transform.localPosition = notOffset.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Horizontal") != 0.0f) {
+			if (Input.GetAxis("Horizontal") < 0.0f) {
+				isOffset = true;
+			} else {
+				isOffset = false;
+			}
+		}
+		if(isOffset) {
+			transform.localPosition = Vector3.Lerp(transform.localPosition, offset.localPosition, (float)(2.0f*Time.deltaTime));
+		} else if(!isOffset) {
+			transform.localPosition = Vector3.Lerp(transform.localPosition, notOffset.localPosition, (float)(2.0f*Time.deltaTime));
+		}
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 10.0F;
+	void Start () {
+	}
+	void Update() {
+		float vert = Input.GetAxis("Vertical") * speed;
+		float hor = Input.GetAxis("Horizontal") * speed;
+		vert *= Time.deltaTime;
+		hor *= Time.deltaTime;
+		transform.Translate(hor, 0, vert);
+	}
+}
